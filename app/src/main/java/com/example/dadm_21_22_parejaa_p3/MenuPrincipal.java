ce20af3547e36b61ea6962d642aa4962e70e2036
miniponti <eleponti.m@outlook.com>
2021-11-10 11:19:09 +0100

    fondo menu principal

---
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p3/MenuPrincipal.java
@@ -0,0 +1,17 @@
+package com.example.dadm_21_22_parejaa_p3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MenuPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // pantalla completa
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.menu_principal);
+    }
+}

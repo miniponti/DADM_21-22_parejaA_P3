e9bbd8580edf19ee4bdc3ca46cf177500753fefa
miniponti <eleponti.m@outlook.com>
2021-11-16 09:59:57 +0100

    menu pausa diseñado

---
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p3/MenuPausaFragment.java
@@ -0,0 +1,29 @@
+package com.example.dadm_21_22_parejaa_p3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+
+public class MenuPausaFragment extends Fragment {
+
+    public MenuPausaFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_menu_pausa, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
--- app/src/main/java/com/example/dadm_21_22_parejaa_p3/MenuPrincipal.java
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p3/MenuPrincipal.java
@@ -2,8 +2,11 @@ package com.example.dadm_21_22_parejaa_p3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -13,5 +16,13 @@ public class MenuPrincipal extends AppCompatActivity {
         // pantalla completa
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.menu_principal);
+
+        Button btn_empezar = findViewById(R.id.btn_empezar);
+        btn_empezar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuPrincipal.this,ActivityJuego.class);
+                startActivity(intent);
+            }
+        });
     }
 }

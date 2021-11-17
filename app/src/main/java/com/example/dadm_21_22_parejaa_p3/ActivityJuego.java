4a51fe6f3faa9a5eae5bef0468fff02bf31758e2
miniponti <eleponti.m@outlook.com>
2021-11-17 10:24:49 +0100

    menu pausa funcional y sonidos metidos

--- app/src/main/java/com/example/dadm_21_22_parejaa_p3/ActivityJuego.java
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p3/ActivityJuego.java
@@ -1,17 +1,57 @@
 package com.example.dadm_21_22_parejaa_p3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.OnLifecycleEvent;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ActivityJuego extends AppCompatActivity {
+
+    FragmentContainerView fragmentContainerView;
+    ImageButton botonPausa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // pantalla completa
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_juego);
+
+        // encontrar fragment container
+        fragmentContainerView = (FragmentContainerView) findViewById(R.id.fragmentContainerView);
+        FragmentManager FM = getSupportFragmentManager();
+        FM.beginTransaction().replace(R.id.fragmentContainerView, new MenuPausaFragment(), "FRAGMENT_PAUSA");
+        fragmentContainerView.setVisibility(View.GONE);
+
+        // boton pausa
+        botonPausa = (ImageButton) findViewById(R.id.btn_pausa);
+        botonPausa.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (fragmentContainerView.getVisibility() == View.GONE){
+                    fragmentContainerView.setVisibility(View.VISIBLE);
+                }
+                else{
+                    fragmentContainerView.setVisibility(View.GONE);
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (fragmentContainerView.getVisibility() == View.GONE){
+            fragmentContainerView.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
--- app/src/main/java/com/example/dadm_21_22_parejaa_p3/MenuPausaFragment.java
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p3/MenuPausaFragment.java
@@ -1,22 +1,37 @@
 package com.example.dadm_21_22_parejaa_p3;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
 
 public class MenuPausaFragment extends Fragment {
+
+    Button botonSalir;
 
     public MenuPausaFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_menu_pausa, container, false);
 
-        return inflater.inflate(R.layout.fragment_menu_pausa, container, false);
+        // BOTON SALIR DEL JUEGO
+        botonSalir = (Button) root.findViewById(R.id.btn_salir);
+        botonSalir.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),MenuPrincipal.class);
+                startActivity(intent);
+            }
+        });
+
+        return  root;
     }
 
     @Override
